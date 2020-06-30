25216fef2f7a0a45f9d2885c0757b12a1b654e5a
David Konsumer <konsumer@jetboystudio.com>
2020-06-30 15:34:06 -0700

    add ignore for node-project, add more editions, add devDep for jest, fix bin

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-const index = require('./index.js')
+const { getInspirationCard } = require('./index.js')
 
 const edition1 = require('./data/edition1.json')
 const edition2 = require('./data/edition2.json')
@@ -9,29 +9,28 @@ global.Math.random = () => 0.5
 
 describe('Get Inspirational Card (edition 1)', () => {
   it('returns a card', () => {
-    const card = index.getInspirationCard(1)
+    const card = getInspirationCard(1)
     expect(edition1).toContain(card)
   })
 })
 
 describe('Get Inspirational Card (edition 2)', () => {
   it('returns a card', () => {
-    const card = index.getInspirationCard(2)
+    const card = getInspirationCard(2)
     expect(edition2).toContain(card)
   })
 })
 
 describe('Get Inspirational Card (edition 3, default)', () => {
   it('returns a card', () => {
-    const card = ind
-    ex.getInspirationCard()
+    const card = getInspirationCard()
     expect(edition3).toContain(card)
   })
 })
 
 describe('Get Inspirational Card (edition 4)', () => {
   it('returns a card', () => {
-    const card = index.getInspirationCard(4)
+    const card = getInspirationCard(4)
     expect(edition4).toContain(card)
   })
 })
