893188240351a2acf76187403b639c39185fbbcd
nessieSnippets <17553527+nessieSnippets@users.noreply.github.com>
2022-01-24 11:57:02 +0100

    Ci/set up actions (#17)
    * chore: adds prettier
    
    * ci: adds ci workflow

--- index.js
+++ index.js
@@ -2,19 +2,19 @@ const cards = [
   require('./data/edition1.json'),
   require('./data/edition2.json'),
   require('./data/edition3.json'),
-  require('./data/edition4.json')
-]
+  require('./data/edition4.json'),
+];
 
 const getRandomInt = (max) => {
-  return Math.floor(Math.random() * Math.floor(max))
-}
+  return Math.floor(Math.random() * Math.floor(max));
+};
 
 const getInspirationCard = (edition = 3) => {
-  cardsList = cards[edition - 1]
-  const randomIndex = getRandomInt(cardsList.length)
-  return cardsList[randomIndex]
-}
+  cardsList = cards[edition - 1];
+  const randomIndex = getRandomInt(cardsList.length);
+  return cardsList[randomIndex];
+};
 
 module.exports = {
-  getInspirationCard
-}
+  getInspirationCard,
+};
